dd0d903f4367b696f1bd2934f74d65f37dfcad0d
Joe Kralicky <joekralicky@gmail.com>
2023-05-23 12:49:57 -0400

    Dependency updates

--- go.mod
+++ go.mod
@@ -5,25 +5,25 @@ go 1.18
 require (
 	github.com/charmbracelet/lipgloss v0.7.1
 	github.com/google/uuid v1.3.0
-	github.com/jhump/protoreflect v1.14.1
+	github.com/jhump/protoreflect v1.15.1
 	github.com/kralicky/gpkg v0.0.0-20220311205216-0d8ea9557555
-	github.com/kralicky/ragu v1.0.0-rc7
-	github.com/magefile/mage v1.14.0
-	github.com/onsi/ginkgo/v2 v2.9.1
-	github.com/onsi/gomega v1.27.4
-	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.40.0
-	go.opentelemetry.io/contrib/propagators/autoprop v0.40.0
-	go.opentelemetry.io/otel v1.14.0
-	go.opentelemetry.io/otel/exporters/jaeger v1.14.0
-	go.opentelemetry.io/otel/metric v0.37.0
-	go.opentelemetry.io/otel/sdk v1.14.0
-	go.opentelemetry.io/otel/sdk/metric v0.37.0
-	go.opentelemetry.io/otel/trace v1.14.0
-	go.uber.org/atomic v1.10.0
+	github.com/kralicky/ragu v1.0.10
+	github.com/magefile/mage v1.15.0
+	github.com/onsi/ginkgo/v2 v2.9.5
+	github.com/onsi/gomega v1.27.7
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.42.0
+	go.opentelemetry.io/contrib/propagators/autoprop v0.42.0
+	go.opentelemetry.io/otel v1.16.0
+	go.opentelemetry.io/otel/exporters/jaeger v1.16.0
+	go.opentelemetry.io/otel/metric v1.16.0
+	go.opentelemetry.io/otel/sdk v1.16.0
+	go.opentelemetry.io/otel/sdk/metric v0.39.0
+	go.opentelemetry.io/otel/trace v1.16.0
+	go.uber.org/atomic v1.11.0
 	go.uber.org/zap v1.24.0
-	golang.org/x/exp v0.0.0-20230315142452-642cacee5cc0
-	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4
-	google.golang.org/grpc v1.53.0
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1
+	google.golang.org/grpc v1.55.0
 	google.golang.org/protobuf v1.30.0
 )
 
@@ -31,17 +31,16 @@ require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/benbjohnson/clock v1.3.0 // indirect
 	github.com/bmatcuk/doublestar v1.3.4 // indirect
+	github.com/bufbuild/protocompile v0.4.0 // indirect
 	github.com/flosch/pongo2/v6 v6.0.0 // indirect
-	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
-	github.com/golang/glog v1.1.0 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/pprof v0.0.0-20230111200839-76d1ae5aea2b // indirect
 	github.com/iancoleman/strcase v0.2.0 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
-	github.com/kralicky/grpc-gateway/v2 v2.15.2 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
@@ -50,17 +49,18 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
-	github.com/samber/lo v1.37.0 // indirect
-	go.opentelemetry.io/contrib/propagators/aws v1.15.0 // indirect
-	go.opentelemetry.io/contrib/propagators/b3 v1.15.0 // indirect
-	go.opentelemetry.io/contrib/propagators/jaeger v1.15.0 // indirect
-	go.opentelemetry.io/contrib/propagators/ot v1.15.0 // indirect
+	github.com/samber/lo v1.38.1 // indirect
+	go.opentelemetry.io/contrib/propagators/aws v1.17.0 // indirect
+	go.opentelemetry.io/contrib/propagators/b3 v1.17.0 // indirect
+	go.opentelemetry.io/contrib/propagators/jaeger v1.17.0 // indirect
+	go.opentelemetry.io/contrib/propagators/ot v1.17.0 // indirect
 	go.uber.org/goleak v1.2.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
-	golang.org/x/mod v0.9.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
-	golang.org/x/tools v0.7.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sync v0.2.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.9.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- metrics.go
+++ metrics.go
@@ -5,61 +5,60 @@ import (
 	"time"
 
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/metric/instrument"
-	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/metric"
 )
 
 type MetricsExporter struct {
-	rxBytesCollector instrument.Int64Counter
-	txBytesCollector instrument.Int64Counter
+	rxBytesCollector metric.Int64Counter
+	txBytesCollector metric.Int64Counter
 
-	rxRpcCollector instrument.Int64Counter
-	txRpcCollector instrument.Int64Counter
+	rxRpcCollector metric.Int64Counter
+	txRpcCollector metric.Int64Counter
 
-	svcRxLatencyCollector instrument.Int64Histogram
-	svcTxLatencyCollector instrument.Int64Histogram
+	svcRxLatencyCollector metric.Int64Histogram
+	svcTxLatencyCollector metric.Int64Histogram
 
 	staticAttrs []attribute.KeyValue
 }
 
-func NewMetricsExporter(provider *metric.MeterProvider, staticAttrs ...attribute.KeyValue) *MetricsExporter {
+func NewMetricsExporter(provider metric.MeterProvider, staticAttrs ...attribute.KeyValue) *MetricsExporter {
 	meter := provider.Meter("github.com/kralicky/totem/metrics")
 
 	rxBytes, err := meter.Int64Counter("stream_receive_bytes",
-		instrument.WithDescription("Total number of bytes received on a stream"))
+		metric.WithDescription("Total number of bytes received on a stream"))
 	if err != nil {
 		panic(err)
 	}
 
 	txBytes, err := meter.Int64Counter("stream_transmit_bytes",
-		instrument.WithDescription("Total number of bytes transmitted on a stream"))
+		metric.WithDescription("Total number of bytes transmitted on a stream"))
 	if err != nil {
 		panic(err)
 	}
 
 	rxRpc, err := meter.Int64Counter("stream_receive_rpcs",
-		instrument.WithDescription("Total number of requests and replies received on a stream"))
+		metric.WithDescription("Total number of requests and replies received on a stream"))
 	if err != nil {
 		panic(err)
 	}
 
 	txRpc, err := meter.Int64Counter("stream_transmit_rpcs",
-		instrument.WithDescription("Total number of requests and replies transmitted on a stream"))
+		metric.WithDescription("Total number of requests and replies transmitted on a stream"))
 	if err != nil {
 		panic(err)
 	}
 
 	svcRxLatency, err := meter.Int64Histogram("stream_local_service_latency",
-		instrument.WithDescription("Incoming RPC request-response latency for services handled locally on a stream"),
-		instrument.WithUnit("μs"),
+		metric.WithDescription("Incoming RPC request-response latency for services handled locally on a stream"),
+		metric.WithUnit("μs"),
 	)
 	if err != nil {
 		panic(err)
 	}
 
 	svcTxLatency, err := meter.Int64Histogram("stream_remote_service_latency",
-		instrument.WithDescription("Outgoing RPC request-response latency for services handled remotely on a stream"),
-		instrument.WithUnit("μs"),
+		metric.WithDescription("Outgoing RPC request-response latency for services handled remotely on a stream"),
+		metric.WithUnit("μs"),
 	)
 	if err != nil {
 		panic(err)
@@ -84,8 +83,8 @@ func (m *MetricsExporter) TrackRxBytes(service, method string, count int64) {
 		attribute.Key("service").String(service),
 		attribute.Key("method").String(method),
 	)
-	m.rxRpcCollector.Add(context.Background(), 1, attrs...)
-	m.rxBytesCollector.Add(context.Background(), count, attrs...)
+	m.rxRpcCollector.Add(context.Background(), 1, metric.WithAttributes(attrs...))
+	m.rxBytesCollector.Add(context.Background(), count, metric.WithAttributes(attrs...))
 }
 
 func (m *MetricsExporter) TrackTxBytes(service, method string, count int64) {
@@ -96,8 +95,8 @@ func (m *MetricsExporter) TrackTxBytes(service, method string, count int64) {
 		attribute.Key("service").String(service),
 		attribute.Key("method").String(method),
 	)
-	m.txRpcCollector.Add(context.Background(), 1, attrs...)
-	m.txBytesCollector.Add(context.Background(), count, attrs...)
+	m.txRpcCollector.Add(context.Background(), 1, metric.WithAttributes(attrs...))
+	m.txBytesCollector.Add(context.Background(), count, metric.WithAttributes(attrs...))
 }
 
 func (m *MetricsExporter) TrackSvcRxLatency(service, method string, latency time.Duration) {
@@ -108,7 +107,7 @@ func (m *MetricsExporter) TrackSvcRxLatency(service, method string, latency time
 		attribute.Key("service").String(service),
 		attribute.Key("method").String(method),
 	)
-	m.svcRxLatencyCollector.Record(context.Background(), latency.Microseconds(), attrs...)
+	m.svcRxLatencyCollector.Record(context.Background(), latency.Microseconds(), metric.WithAttributes(attrs...))
 }
 
 func (m *MetricsExporter) TrackSvcTxLatency(service, method string, latency time.Duration) {
@@ -119,5 +118,5 @@ func (m *MetricsExporter) TrackSvcTxLatency(service, method string, latency time
 		attribute.Key("service").String(service),
 		attribute.Key("method").String(method),
 	)
-	m.svcTxLatencyCollector.Record(context.Background(), latency.Microseconds(), attrs...)
+	m.svcTxLatencyCollector.Record(context.Background(), latency.Microseconds(), metric.WithAttributes(attrs...))
 }
